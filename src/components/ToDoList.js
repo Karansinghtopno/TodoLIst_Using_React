7b1e1737b8b32d7a294a9df2b24efb8745da38fd
karan <karansinghtopno@gmail.com>
2022-08-28 02:27:39 +0530

    first commit

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,105 +1,83 @@
-import React, { useState, useEffect } from "react";
-import todo from "../images/todo-1.jpg";
-import "./ToDoList.css";
-// import {FaHandPeace} from 'react-icons/fa'
+import React from "react";
+import { useState} from "react";
+import todo from "../images/to-do-list.svg"
 
-const getListFromLocal=()=>{
-  let list =localStorage.getItem("todolist");
-  // console.log(list);
-  if(list){
-    return  JSON.parse( localStorage.getItem("todolist"));
-  }
-  else return [];
-} 
+function ToDoList() {
+  const [inputData,setInputData]= useState("");
+  const [items,setItems]=useState([]);
 
-const ToDoList = () => {
-  //state for input data
-  const [inputData, setInputData] = useState("");
 
-  //state for storing data
-  const [items, setItems] = useState(getListFromLocal());
-
-  //function for adding the items to an empty array
+  //Add Item
   const addItem = () => {
-    if (!inputData) return;
-    const listObject={ id:new Date().getTime().toString(), list:inputData}
-    setItems([...items, listObject]);
-    setInputData("");
-    // console.log(listObject);
-  };
+    if(!inputData){
 
-  //deleting an item in an item list
-  const deleteItem = (index) => {
-    console.log("removing from index: ", index);
-    const updatedItems = items.filter((element) => {
-      return index !== element.id;
-    });
+    }else{
+      setItems([...items,inputData]);
+      setInputData("");
+    }
+    
+  }
+  //Delete Item
+  //id of an element of the list that is array
+  const deleteItem=(id)=>{
+    console.log(id);
+
+    const updatedItems = items.filter((element,index)=>{
+      console.log(element);
+      return index !== id;
+    })
+    // console.log(updatedItems);
     setItems(updatedItems);
-  };
+    console.log(items);
 
-  //removing all the items at once
-  const removeAll = () => {
+  };
+  const removeAll=()=>{
     setItems([]);
-  };
+  }
 
-  //add data to local storage
-   //local storege always store in key, value pair
-    // keys & value is always in string format
-  useEffect(() => {
-   
-    localStorage.setItem("todolist", JSON.stringify(items))
-  }, [items]);
 
-  return (
-    <>
+
+  return <>
       <div className="main-div">
         <div className="child-div">
           <figure>
-            <img className="image" src={todo} alt="todo logo" />
-            <figcaption>
-              {" "}
-              Add Your List Here{" "}
-              <span>
-                <i class="fa-solid fa-pen-clip"></i>
-              </span>
-            </figcaption>
+            <img src={todo} alt="todo_logo" />
+            <figcaption>Add Your Text Here 👇👇</figcaption>
           </figure>
+
           <div className="addItems">
-            <input
-              type="text"
-              placeholder="Add Items..."
-              value={inputData}
-              onChange={(e) => {
-                setInputData(e.target.value);
-              }}
+            <input type="text" placeholder=' ✍️ Add Items.... '
+            value={inputData}
+            onChange={(e)=> setInputData(e.target.value)}
             />
-            <i
-              title="Add Item"
-              class="fa-solid fa-plus icon"
-              onClick={addItem}
-            ></i>
+            <i className=" fa fa-plus add-btn" title="Add Item" onClick={addItem}></i>
+            
           </div>
+
           <div className="showItems">
-            {items.map((element) => {
-              return (
-                <div className="eachItem" key={element.id}>
-                  <h3>{element.list}</h3>
-                  <i
-                    title="Delete Item"
-                    class="fa-solid fa-trash-can"
-                    onClick={() => deleteItem(element.id)}
-                  ></i>
-                </div>
-              );
-            })}
+            {
+              items.map((element,ind)=>{
+                return(
+                  <div className="eachItem" key={ind}>
+                  <h3>{element}</h3>
+                  <i className="fa fa-trash-alt add-btn" title="Delete Item" onClick={()=>deleteItem(ind)} ></i>
+                  </div>
+
+                );
+              })
+            }
+
+            
+            
           </div>
-          <div className="button-div">
-            <button onClick={removeAll}> Remove All</button>
+
+          <div className="showItems">
+            <button className="btn effect04" data-sm-link-text="Remove All" onClick={removeAll} ><span>CHECK LIST</span> </button>
           </div>
+          
         </div>
       </div>
-    </>
-  );
-};
+  </>;
+}
 
 export default ToDoList;
